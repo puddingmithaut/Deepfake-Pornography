b50e5e34423afd129e9b6b2ce6c53680d86c725a
puddingmithaut <czakallalaura@gmail.com>
2026-05-08 19:26:06 +0200

    ouf 3 kreise

--- sketch.js
+++ sketch.js
@@ -20,20 +20,25 @@ function setup() {
 function draw() {
 
 drawPiechartone();
+drawPiecharttwo();
+drawPiechartthree();
 
 }
 
-let segmente = []; 
-let farben = ["#F44336", "#673AB7", "#03A9F4"];
-let werte = [0.02, 0.98];
 
 function drawPiechartone() {
+
+  let segmente = []; 
+  let farben = ["#F44336", "#673AB7", "#03A9F4"];
+  let werte = [0.02, 0.98];
+
   let arcX = windowWidth/5.8;
   let arcY = windowHeight/1.41;
   let arcS = windowWidth/4.5;
   
   let startwinkel = -HALF_PI/1.6;
   segmente = []; 
+  
   
   for (let i = 0; i < werte.length; i++) {
     let winkel = werte[i] * TWO_PI;
@@ -54,16 +59,11 @@ function drawPiechartone() {
     }
     arc(arcX, arcY, arcS, arcS, segmente[i].start, segmente[i].ende, PIE);
   }
-  
-  let hoverSegment = getHoverSegment(arcX, arcY, arcS);
-  if (hoverSegment !== -1 && werte[hoverSegment] !== undefined) {
-    drawInfobox(werte[hoverSegment] * 100 + "%");
-  }
-}
 
-function getHoverSegment(arcX, arcY, arcS) {
-  let abstand = dist(mouseX, mouseY, arcX, arcY);
-  if (abstand > arcS / 2) {
+
+  function getHoverSegment(arcX, arcY, arcS) {
+   let abstand = dist(mouseX, mouseY, arcX, arcY);
+    if (abstand > arcS / 2) {
     return -1;
   }
   
@@ -73,14 +73,13 @@ function getHoverSegment(arcX, arcY, arcS) {
     mausWinkel += TWO_PI;
   }
   
-  // Wichtig: Startwinkel von -HALF_PI (oben) auf 0 bis TWO_PI umrechnen
-  let angepassterMausWinkel = (mausWinkel + HALF_PI/1.6) % TWO_PI;
+    // Wichtig: Startwinkel von -HALF_PI (oben) auf 0 bis TWO_PI umrechnen
+    let angepassterMausWinkel = (mausWinkel + HALF_PI/1.6) % TWO_PI;
   
-  for (let i = 0; i < segmente.length; i++) {
+    for (let i = 0; i < segmente.length; i++) {
     let start = segmente[i].start + HALF_PI/1.6;
     if (start < 0) start += TWO_PI;
-    
-    let ende = segmente[i].ende + HALF_PI;
+    let ende = segmente[i].ende + HALF_PI/1.6;
     if (ende < 0) ende += TWO_PI;
     
     // Prüfen ob Mauswinkel im Segment liegt
@@ -98,21 +97,158 @@ function getHoverSegment(arcX, arcY, arcS) {
   return -1;
 }
 
-function drawInfobox(label) {
-  fill(50);
-  rect(mouseX + 10, mouseY - 30, 70, 25);
-  fill(255);
-  textSize(12);
-  text(label, mouseX + 15, mouseY - 13);
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
-
 function drawPiecharttwo(){
+  let segmente = []; 
+  let farben = ["#F44336", "#673AB7", "#03A9F4"];
+  let werte = [0.01, 0.99];
 
+  let arcX = windowWidth/1.64;
+  let arcY = windowHeight/3.4;
+  let arcS = windowWidth/5;
+  
+  let startwinkel = -HALF_PI/1.6;
+  segmente = []; 
+  
+  
+  for (let i = 0; i < werte.length; i++) {
+    let winkel = werte[i] * TWO_PI;
+    segmente.push({
+      start: startwinkel,
+      ende: startwinkel + winkel,
+      wert: werte[i],
+    });
+    startwinkel += winkel;
+  }
+  
+  // Segmente zeichnen
+  for (let i = 0; i < segmente.length; i++) {
+    if (i === getHoverSegment(arcX, arcY, arcS)) {
+      fill(255);
+    } else {
+      fill(farben[i]);
+    }
+    arc(arcX, arcY, arcS, arcS, segmente[i].start, segmente[i].ende, PIE);
+  }
+  function getHoverSegment(arcX, arcY, arcS) {
+  let abstand = dist(mouseX, mouseY, arcX, arcY);
+  if (abstand > arcS / 2) {
+    return -1;
+  }
+  
+  let mausWinkel = atan2(mouseY - arcY, mouseX - arcX);
+  // Umwandeln in Bereich 0 bis TWO_PI
+  if (mausWinkel < 0) {
+    mausWinkel += TWO_PI;
+  }
+  
+  // Wichtig: Startwinkel von -HALF_PI (oben) auf 0 bis TWO_PI umrechnen
+  let angepassterMausWinkel = (mausWinkel + HALF_PI/1.6) % TWO_PI;
+  
+  for (let i = 0; i < segmente.length; i++) {
+    let start = segmente[i].start + HALF_PI/1.6;
+    if (start < 0) start += TWO_PI;
+    let ende = segmente[i].ende + HALF_PI/1.6;
+    if (ende < 0) ende += TWO_PI;
+    
+    // Prüfen ob Mauswinkel im Segment liegt
+    if (start < ende) {
+      if (angepassterMausWinkel >= start && angepassterMausWinkel < ende) {
+        return i;
+      }
+    } else {
+      // Über den 2π-Grenzfall
+      if (angepassterMausWinkel >= start || angepassterMausWinkel < ende) {
+        return i;
+      }
+    }
+  }
+  return -1;
 
+}
 }
 
 function drawPiechartthree(){
+let segmente = []; 
+  let farben = ["#F44336", "#673AB7", "#03A9F4"];
+  let werte = [0.02, 0.98];
 
+  let arcX = windowWidth/1.408 ;
+  let arcY = windowHeight/1.33;
+  let arcS = windowWidth/5;
+  
+  let startwinkel = -HALF_PI/1.6;
+  segmente = []; 
+  
+  
+  for (let i = 0; i < werte.length; i++) {
+    let winkel = werte[i] * TWO_PI;
+    segmente.push({
+      start: startwinkel,
+      ende: startwinkel + winkel,
+      wert: werte[i],
+    });
+    startwinkel += winkel;
+  }
+  
+  // Segmente zeichnen
+  for (let i = 0; i < segmente.length; i++) {
+    if (i === getHoverSegment(arcX, arcY, arcS)) {
+      fill(255);
+    } else {
+      fill(farben[i]);
+    }
+    arc(arcX, arcY, arcS, arcS, segmente[i].start, segmente[i].ende, PIE);
+  }
+
+
+  function getHoverSegment(arcX, arcY, arcS) {
+   let abstand = dist(mouseX, mouseY, arcX, arcY);
+    if (abstand > arcS / 2) {
+    return -1;
+  }
+  
+  let mausWinkel = atan2(mouseY - arcY, mouseX - arcX);
+  // Umwandeln in Bereich 0 bis TWO_PI
+  if (mausWinkel < 0) {
+    mausWinkel += TWO_PI;
+  }
+  
+    // Wichtig: Startwinkel von -HALF_PI (oben) auf 0 bis TWO_PI umrechnen
+    let angepassterMausWinkel = (mausWinkel + HALF_PI/1.6) % TWO_PI;
+  
+    for (let i = 0; i < segmente.length; i++) {
+    let start = segmente[i].start + HALF_PI/1.6;
+    if (start < 0) start += TWO_PI;
+    let ende = segmente[i].ende + HALF_PI/1.6;
+    if (ende < 0) ende += TWO_PI;
+    
+    // Prüfen ob Mauswinkel im Segment liegt
+    if (start < ende) {
+      if (angepassterMausWinkel >= start && angepassterMausWinkel < ende) {
+        return i;
+      }
+    } else {
+      // Über den 2π-Grenzfall
+      if (angepassterMausWinkel >= start || angepassterMausWinkel < ende) {
+        return i;
+      }
+    }
+  }
+  return -1;
+}
 
 }
